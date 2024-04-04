bfff814071a3bbbb24fc5394daa2295f86d1b5c0
Kazuko Murata <kazuko.murata@irologue.com>
2024-04-04 11:30:26 +0900

    ミドルウェア追加

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware([\App\Http\Middleware\SampleMiddleware::class]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +16,21 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+  ->middleware('auth')
+  ->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
+  ->name('tweet.update.index')
+  ->where('tweetId', '[0-9]+');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
+  ->name('tweet.update.put')
+  ->where('tweetId', '[0-9]+');
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
+  ->name('tweet.delete')
+  ->where('tweetId', '[0-9]+');
 
 require __DIR__.'/auth.php';
