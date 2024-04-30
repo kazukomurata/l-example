82a4d5017324c8fbf33ffb29bdefba932f482833
Kazuko Murata <kazuko.murata@irologue.com>
2024-04-30 16:23:27 +0900

    メール設定

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     ],
 
     'mailgun' => [
-        'domain' => env('MAILGUN_SMTP_SERVER'),
+        'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
         'scheme' => 'https',
